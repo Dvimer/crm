693b3825660c2522654b545c676b0994cfdd7ad4
Алексей Носов <anosov@seven.tech>
2020-11-03 11:01:01 +0300

    добавил добавление машины

--- src/main/java/com/crm/car/web/controller/CarController.java
+++ src/main/java/com/crm/car/web/controller/CarController.java
@@ -6,9 +6,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 @RequiredArgsConstructor
@@ -21,5 +24,18 @@ public class CarController {
         List<Car> cars = carRepository.findAll();
         theModel.addAttribute("cars", cars);
         return "car/list";
+    }
+
+    @GetMapping("/showFormForAdd")
+    public String addCar(Model theModel) {
+        Car car = Car.builder().id(UUID.randomUUID()).build();
+        theModel.addAttribute("car", car);
+        return "car/form-add";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute("car") Car car) {
+        carRepository.save(car);
+        return "redirect:/cars/list";
     }
 }
